e7212910cc1a11f1c202acffd9fd20810a398e1b
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-07 07:48:56 +0800

    2018 Updates

---
+++ Conversions/problem13.js
@@ -0,0 +1,6 @@
+//Problem 13: Write a program that converts celsius to farenheit.
+var celsius = 10.0;
+var farenheit = 0.0;
+farenheit = (9.0 / 5.0) * celsius + 32;
+console.log(celsius.toString() + "C equals to " +
+ farenheit.toString() + "F");
