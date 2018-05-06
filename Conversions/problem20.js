2aa7689a311dadafb68503cef3bf84cadfcd430e
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-07 07:50:30 +0800

    2018 Updates

---
+++ Conversions/problem20.js
@@ -0,0 +1,8 @@
+//Problem 20: Write a program that converts a number to its 
+//corresponding month (e.g. 1 = January).
+var index = 1;
+var month = ["January", "February", "March", "April",
+ "May", "June", "July", "August",
+ "September", "October", "November", "December"];
+
+console.log(month[index - 1]);
