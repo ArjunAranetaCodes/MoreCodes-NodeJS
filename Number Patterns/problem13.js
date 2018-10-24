63c37fc36ada2a6ddaa41de0a4744f73d9a20987
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-10-24 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/problem13.js
@@ -0,0 +1,29 @@
+/*
+Problem 13: Write a program to print the pattern of asterisks using loop.
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+*/
+var y = 0;
+var row = 5;
+for(y = 0; y <= row; y++){
+ var x = 0;
+ for(x = 0; x < y; x++){
+  process.stdout.write("*");
+ }
+ process.stdout.write("\n");
+}
+
+for(y = row - 1; y > 0; y--){
+ var x = 0;
+ for(x = 0; x < y; x++){
+  process.stdout.write("*");
+ }
+ process.stdout.write("\n");
+}
